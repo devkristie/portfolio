a560fe38ba4f6989747895a7c98314942ba88c65
Kristie <kristie_larke@outlook.com>
2024-03-16 14:08:11 +0000

    Added the script tag. Added the click event for the top navigation bar.
    Added the JavaScript script tag to the head of the document with the attribute defer.
    Added the script for the top navigation bar to toggle visible and hide when the hamburger menu is clicked.
    Added overflow-x hidden to the CSS body selector to stop the horizontal overflow.
    Added another CSS selector to hide the navigation bar when toggled with JavaScript.

---
+++ src/js/script.js
@@ -0,0 +1,9 @@
+const hamburgerMenuIcon = document.querySelector(".hamburger-menu");
+const topNavBarShow = document.querySelector(".accordion-navigation-bar-hide");
+
+hamburgerMenuIcon.addEventListener("click", showMenu);
+
+function showMenu() {
+    topNavBarShow.classList.toggle("accordion-navigation-bar-hide");
+    topNavBarShow.classList.toggle("accordion-navigation-bar");
+}
