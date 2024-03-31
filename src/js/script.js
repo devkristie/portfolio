23ba82e60d038eaa6c659d6ec02cb238221bcb32
Kristie <kristie_larke@outlook.com>
2024-03-31 15:48:27 +0100

    Added a class of hidden to the p elements on the  portfolio website accordions. Added the JavaScript for the show more accordion toggle.  Resized and repositioned the view buttons. Styled the accordion. Removed unnecessary properties.
    Added a class of hidden to the p elements on the  portfolio website accordions.
    Added the JavaScript for the show more accordion toggle.
    Resized and repositioned the view buttons.
    Styled the accordion. Removed unnecessary properties.

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,18 @@
 const hamburgerMenuIcon = document.querySelector(".hamburger-menu");
 const topNavBarShow = document.querySelector(".accordion-navigation-bar-hide");
+const showMoreButton = document.querySelector(".show-more-arrow");
+const showMore = document.querySelector(".show-more-accordion");
 
 hamburgerMenuIcon.addEventListener("click", showMenu);
 
 function showMenu() {
     topNavBarShow.classList.toggle("accordion-navigation-bar-hide");
     topNavBarShow.classList.toggle("accordion-navigation-bar");
+}
+
+showMoreButton.addEventListener("click", showInformation);
+
+function showInformation() {
+    showMore.classList.toggle("show-more-accordion");
+    showMore.classList.toggle("portfolio-website-content-container");
 }
