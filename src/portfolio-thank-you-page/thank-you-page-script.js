c5289496f99eceed27f10f8e6c30c8a35419c611
Kristie <kristie_larke@outlook.com>
2024-05-20 20:30:11 +0100

    Added animation and links to the navigation.
    Added the animation for the thank you message.
    Added an icon to the thank you page.
    Added the links to the hypertext ref attributes on the thank you page.
    Added the width and height of the thank you message SVG image to the external CSS file.
    Added a pointer cursor and filter to the button hover.

--- src/portfolio-thank-you-page/thank-you-page-script.js
+++ src/portfolio-thank-you-page/thank-you-page-script.js
@@ -7,3 +7,33 @@ function showMenu() {
   topNavBarShow.classList.toggle("accordion-navigation-bar-hide");
   topNavBarShow.classList.toggle("accordion-navigation-bar");
 }
+
+const thankYouWriting = document.querySelectorAll(".thank-you-writing1");
+const thankYouWritingLine = document.querySelectorAll(".thank-you-writing2");
+
+document.addEventListener("DOMContentLoaded", function() {
+  function isInViewport(element) {
+    const rect = element.getBoundingClientRect();
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  }
+
+  thankYouWriting.forEach((element, index) => {
+    if (isInViewport(element)) {
+      setTimeout(() => {
+        element.style.animation = "draw 10s linear 2s forwards";
+
+        // After the animation of the first element is finished, trigger the animation of the second element
+        element.addEventListener("animationend", function() {
+          thankYouWritingLine.forEach((lineElement) => {
+            lineElement.style.animation = "draw 3s linear 5s forwards";
+          });
+        });
+      }, index * 1250);
+    }
+  });
+});
